0e0299e271302f1633ae0ca2e7d7caa60688027f
gwy123a <59083639+gwy123a@users.noreply.github.com>
2020-03-16 00:04:36 +0200

    Add files via upload

---
+++ single_list.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef struct slist
+{
+	int id ;
+	struct slist *next ;			
+}L;
+ 
+//create a node 
+L *create_node(int data)
+{
+	//Allocate the same amount of space to each node 
+	L *p = malloc(sizeof(L));
+	if(NULL == p)
+	{
+		printf("malloc error!\n");
+		return NULL ;
+	}
+	//Clean structure 
+	memset(p,0,sizeof(L));
+	//Initialize the first node 
+	p->id = data ; 
+	//Set the node's successor pointer to NULL 
+	p->next = NULL ;
+}
+ 
+//Tail insertion of linked list 
+void tail_insert(L *pH , L *new)
+{
+	//Get the current position 
+	L *p = pH ; 
+	//If the next node at the current position is not NUll 
+	while(NULL != p->next)
+	{
+		//Move to the next node 
+		p = p->next ;
+	}
+	//If you jump out of the above loop, you have reached the position of NULL
+	//At this time, the newly inserted node is directly assigned to the NULL position. 
+	p->next = new ;
+}
+ 
+//Head insertion of linked list 
+void top_insert(L *pH , L *new)
+{
+	L *p = pH ;
+	new->next = p->next ;
+	p->next = new ;
+}
+ 
+//Traverse of linked list 
+void Print_node(L *pH)
+{
+	//Get the current position 
+	L *p = pH ;
+	//Get the position of the first node 
+	p = p->next ;
+	//If the next node at the current position is not null 
+	while(NULL != p->next)
+	{
+		//(1)Print node value 
+		printf("id:%d\n",p->id);
+		//(2)Move to next node,if condition is still true，repeat(1)，then(2) 
+		p = p->next ;
+	}
+	//If the next node at the current position is null, print the value
+	//means there is only one node 
+	printf("id:%d\n",p->id);
+}
+ 
+//Delete node in Linked list 
+int detele_list_node(L * pH , int data)
+{
+	//Get the position of the current head node 
+	L *p = pH ;
+	L *prev = NULL;
+	while(NULL != p->next)
+	{
+		//Holds a pointer to the previous node of the current node 
+		prev = p ;
+		//Then let the current pointer move forward 
+		p = p->next ; 	
+		//Determine if the data need to be deleted is found  
+		if(p->id == data)
+		{
+			//Two cases, one is a Middle node, and the other is a tail node
+			if(p->next != NULL)  //Middle node 
+			{
+				prev->next = p->next ;
+				free(p);//Free
+			}
+			else //Tail node 
+			{
+				prev->next = NULL ; //Let the previous node of the tail node pointer to null 
+				free(p); // Free
+			}
+			return 0  ;
+		}
+	}
+	printf("No found of the node\n");
+	return -1 ;
+}
+ 
+
+int main(int argc , char **argv) 
+{
+	//create first node 
+	int i ;
+	L *header = create_node(0); 
+	for(i = 1 ; i < 10 ; i++)
+	{
+		tail_insert(header,create_node(i));
+	}
+	Print_node(header);
+	detele_list_node(header,5);
+	putchar('\n');
+	Print_node(header);
+	putchar('\n');
+	trave_list(header);
+	Print_node(header);
+	return 0 ;
+}
